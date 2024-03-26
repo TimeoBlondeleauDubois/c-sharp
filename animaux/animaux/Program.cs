758c7a027b5b6b0ac9eca55e32520494c23f896e
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-03-26 09:31:40 +0100

    test

---
+++ animaux/animaux/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Animaux
+{
+    static void Main(string[] args)
+    {
+       
+    }
+
+    public class Animal
+    {
+        public string name;
+
+        public void Move()
+        {
+            Console.WriteLine("Moove");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+}
