09c93cf0e7d7c3cfea7dd3170b7e046dc2ba69c5
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-03-28 12:02:31 +0100

    azdf

---
+++ superhero/superhero/Models/Hero.cs
@@ -0,0 +1,21 @@
+﻿namespace superhero.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+
+        public string Power { get; set; }
+
+        public Hero()
+        {
+            Name = "Unknown";
+            Power = "Unknown";
+        }
+
+        public Hero(string name, string power)
+        {
+            Name = name;
+            Power = power;
+        }
+    }
+}
