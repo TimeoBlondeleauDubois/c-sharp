9343dfba6ff63ae6bbb5218e8624c6d26351b529
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-08-27 09:37:38 +0200

    Hodor

--- ASPNET/ASPNET/Controllers/HomeController.cs
+++ ASPNET/ASPNET/Controllers/HomeController.cs
@@ -23,6 +23,17 @@ namespace ASPNET.Controllers
             return View();
         }
 
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
