d5aa96d401a65788b65559822f761ecf16a06893
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-08-28 16:27:47 +0200

    Update role

--- Livre/Livre/Program.cs
+++ Livre/Livre/Program.cs
@@ -5,19 +5,27 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+// Ajout de la chaîne de connexion
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<FilmContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Ajout des services d'identité avec support des rôles
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()  // Ajout du support des rôles
     .AddEntityFrameworkStores<FilmContext>();
-builder.Services.AddControllersWithViews();
+
+// Ajout du service FilmServices
 builder.Services.AddScoped<IFilmServices, FilmServices>();
+
+// Configuration des contrôleurs avec vues
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuration du pipeline de requête HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -25,7 +33,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -36,9 +43,31 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Configuration des routes
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+// Ajout des rôles au démarrage de l'application
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await EnsureRolesAsync(roleManager);
+}
+
 app.Run();
+
+// Méthode pour s'assurer que les rôles existent
+static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+{
+    string[] roleNames = { "Vendeur", "Acheteur", "Administrateur" };
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
